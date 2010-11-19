4fd68d2530edba2325111f77a0f19e5ed734421a
Kyle Fox <kyle.fox@gmail.com>
2010-11-19 12:59:03 -0700

    Fixes issue #1: lack of AWS environment variables was breaking bin/kirby

--- kirby/publisher.py
+++ kirby/publisher.py
@@ -3,10 +3,6 @@ import cStringIO, gzip
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-KIRBY_BUCKET = os.environ['KIRBY_BUCKET']
-
 def _gzip(data):
     zbuf = cStringIO.StringIO()
     zfile = gzip.GzipFile(mode='wb', compresslevel=6, fileobj=zbuf)
@@ -15,18 +11,19 @@ def _gzip(data):
     return zbuf.getvalue()
 
 def publish(site):
-    pages = site.s3_page_dict()
+    
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    KIRBY_BUCKET = os.environ['KIRBY_BUCKET']
     s3 = S3Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    
     bucket = s3.get_bucket(KIRBY_BUCKET)
     headers = {
         'Content-Type': 'text/html',
         'Content-Encoding': 'gzip'
     }
-    for path, content in pages.items():
+    for path, content in site.s3_page_dict().items():
         k = Key(bucket)
         k.key = path
         k.set_contents_from_string(_gzip(content), headers)
         k.set_acl('public-read')
-
-if __name__ == "__main__":
-    publish()
