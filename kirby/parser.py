e6fd106bc685ed5714cc1e9907b98ab6a3de20a7
Jonathan Smelquist <jsmelquist@jMac.local>
2010-11-18 20:21:47 -0700

    cleaning up

--- kirby/parser.py
+++ kirby/parser.py
@@ -6,18 +6,27 @@ from jinja2 import *
 from kirby import settings
 
 class Page():
-    
-    def __init__(self, page):
-        self.template = "page.html"
-        content = open(os.path.join(settings.CONTENT_DIR, page)).read()
-        data = yaml.load(content)
-        data['content'] = data['content'].replace('\n', '\n\n')
-        data['content'] = markdown.markdown(data['content'])
-        self.data = data
-        for p in data.keys():
-            self.__dict__[p] = data[p]
-            
-    def render(self):
-        env = Environment(loader=FileSystemLoader(settings.TEMPLATE_DIRS))
-        template = env.get_template(self.template)
-        return template.render(self.data)
+
+  def __init__(self, page):
+    """
+    Initializes the pag object with data loaded from a
+    markdown file.
+    """
+    self.template = "page.html"
+    content = open(os.path.join(settings.CONTENT_DIR, page)).read()
+    data = yaml.load(content)
+    data['content'] = data['content'].replace('\n', '\n\n')
+    data['content'] = markdown.markdown(data['content'])
+    self.data = data
+    for p in data.keys():
+        self.__dict__[p] = data[p]
+          
+  def render(self):
+    """
+    Renders out the page object to a template, and return the 
+    resulting html. Pages will attempt to use `page.html` as the 
+    template name which can be overridden in the markdown file.
+    """
+    env = Environment(loader=FileSystemLoader(settings.TEMPLATE_DIRS))
+    template = env.get_template(self.template)
+    return template.render(self.data)
