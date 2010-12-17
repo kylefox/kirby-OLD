c012399842b90020f4016764273497d8c5d06ff6
Kyle Fox <kyle.fox@gmail.com>
2010-12-16 20:28:06 -0700

    Better organization of tests, added some tests for nested vs. simple pages.

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -10,29 +10,47 @@ class PageTest(unittest.TestCase):
         self.KIRBY_ROOT = os.path.dirname(kirby.__file__)
         self.site = Kirby(os.path.join(self.KIRBY_ROOT, 'site_template'))
         self.index = Page(self.site, os.path.join(self.site.content_path, 'index.md'))
-        self.page = Page(self.site, os.path.join(self.site.content_path, 'posts', 'example-post.md'))
-        
+        self.page = Page(self.site, os.path.join(self.site.content_path, 'about.md'))
+        self.nested_page = Page(self.site, os.path.join(self.site.content_path, 'posts', 'example-post.md'))
+
+    # Page URLs:
+
     def test_page_url(self):
-        self.assertEqual(self.page.url, '/posts/example-post')
+        self.assertEqual(self.page.url, '/about')
+        
+    def test_nested_page_url(self):
+        self.assertEqual(self.nested_page.url, '/posts/example-post')
         
     def test_index_url(self):
         self.assertEqual(self.index.url, '/')
         
+    # S3 Keys:    
+        
     def test_page_s3_key(self):
-        self.assertEqual(self.page.s3_key, 'posts/example-post')
+        self.assertEqual(self.page.s3_key, 'about')    
+    
+    def test_nested_page_s3_key(self):
+        self.assertEqual(self.nested_page.s3_key, 'posts/example-post')
         
     def test_index_s3_key(self):
         self.assertEqual(self.index.s3_key, 'index')
-
-    def test_default_template(self):
-        self.assertEqual(self.page.template, 'page.html')
-        
-    def test_custom_template(self):
-        page = Page(self.site, os.path.join(self.site.content_path, 'blog.md'))
-        self.assertEqual(page.template, 'blog.html')
+    
+    # HTML filesystem paths (for rendered content)
         
     def test_page_html_path(self):
-        self.assertEqual(self.page.html_path, 'posts/example-post/index.html')
+        self.assertEqual(self.page.html_path, 'about/index.html')
+        
+    def test_nested_page_html_path(self):
+        self.assertEqual(self.nested_page.html_path, 'posts/example-post/index.html')
         
     def test_index_html_path(self):
         self.assertEqual(self.index.html_path, 'index.html')
+
+    # Templates
+        
+    def test_default_template(self):
+        self.assertEqual(self.page.template, 'page.html')
+
+    def test_custom_template(self):
+        page = Page(self.site, os.path.join(self.site.content_path, 'blog.md'))
+        self.assertEqual(page.template, 'blog.html')
