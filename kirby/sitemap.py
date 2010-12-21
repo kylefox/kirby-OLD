5194422733fa93facd0f102597f046df3316da33
Kyle Fox <kyle.fox@gmail.com>
2010-12-20 21:26:33 -0700

    Stubbed in a sitemap function

---
+++ kirby/sitemap.py
@@ -0,0 +1,19 @@
+from jinja2 import *
+from kirby.template.default_filters import filters
+
+SITEMAP = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">{% for page in pages %}
+   <url>
+      <loc>http://example.com{{page.url}}</loc>
+      <lastmod>{{page.modified_at|datetimeformat("%Y-%m-%d")}}</lastmod>
+      {% if page.changefreq %}<changefreq></changefreq>{% endif %}
+      {% if page.priority %}<priority></priority>{% endif %}
+   </url>
+{% endfor %}</urlset>"""
+
+def sitemap(site):
+    """Returns an XML sitemap for the site."""
+    env = Environment()
+    env.filters.update(filters)
+    template = env.from_string(SITEMAP)
+    return template.render(pages=site.pages.values())
