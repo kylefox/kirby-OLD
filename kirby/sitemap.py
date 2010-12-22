e004e1f9b77da629e47e22b285ad32216acd127c
Kyle Fox <kyle.fox@gmail.com>
2010-12-21 20:51:55 -0700

    escape urls in sitemap xml

--- kirby/sitemap.py
+++ kirby/sitemap.py
@@ -4,7 +4,7 @@ from kirby.template.default_filters import filters
 SITEMAP = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">{% for page in site.pages.values() %}
    <url>
-      <loc>http://{{site.config.domain}}{{page.url}}</loc>
+      <loc>http://{{site.config.domain|escape}}{{page.url|escape}}</loc>
       <lastmod>{{page.modified_at|datetimeformat("%Y-%m-%d")}}</lastmod>
       {% if page.changefreq %}<changefreq></changefreq>{% endif %}
       {% if page.priority %}<priority></priority>{% endif %}
